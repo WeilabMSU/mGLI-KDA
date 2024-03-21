0fcc0e7c46bb6a0b93c31c358eca9bc7b5256af2
Hongsong Feng <hongsongfeng@Hongsongs-Mac-mini.local>
2024-03-20 20:46:29 -0400

    add

--- copy-pdb.py
+++
@@ -1,9 +0,0 @@
-import os
-import pandas as pd
-
-datasets = ["large", "medium", "small"]
-
-for dataset in datasets:
-    PDBs = pd.read_csv(f"datasets/Set-364/{dataset}set.csv", header=0)["PDBID"]
-    for PDB in PDBs:
-        os.system(f"cp datasets/Set-364/{PDB}_CA_A2.pdb datasets/Bfactor-{dataset}")
